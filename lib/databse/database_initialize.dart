956b49c67ff6ee0c5e0022b96ad93e23cae50eee
rasha <mrasha141@gmail.com>
2024-12-24 14:28:40 +0530

    changes added in db qry

--- lib/databse/database_initialize.dart
+++ lib/databse/database_initialize.dart
@@ -1,21 +1,20 @@
-
 import 'dart:developer';
 
 import 'package:path/path.dart';
 import 'package:play_video/databse/database_model.dart';
 import 'package:sqflite/sqflite.dart';
 
-  late Database videoDatabase;
-  bool _isDatabaseInitialized = false;
-  Future<void> initialiseDb() async {
+late Database videoDatabase;
+bool _isDatabaseInitialized = false;
+Future<void> initialiseDb() async {
   if (!_isDatabaseInitialized) {
-   final databasePath = await getDatabasesPath();
-   final path = join(databasePath, 'auction.db');
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, 'auction.db');
 
-   log('opened...');
-   videoDatabase = await openDatabase(path, version: 1,
-    onCreate: (Database db, int version) async {
-      await db.execute(''' CREATE TABLE videos (
+    log('opened...');
+    videoDatabase = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute(''' CREATE TABLE IF NOT EXISTS videos (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           title TEXT,
           description TEXT,
@@ -23,26 +22,25 @@ import 'package:sqflite/sqflite.dart';
           createdAt TEXT) ''');
     });
     _isDatabaseInitialized = true;
-   }
   }
+}
 
-  Future<void> insertVideo(VideoModel video) async {
-    await initialiseDb();
-    final db = videoDatabase;
-    await db.insert('videos', video.toMap());
-  }
+Future<void> insertVideo(VideoModel video) async {
+  await initialiseDb();
+  final db = videoDatabase;
+  await db.insert('videos', video.toMap());
+}
 
-  Future<List<VideoModel>> getAllVideos() async {
-    await initialiseDb();
-    final db = videoDatabase;
-    final List<Map<String, dynamic>> maps = await db.query('videos');
-    return List.generate(maps.length, (i) {
-      return VideoModel(
-        title: maps[i]['title'],
-        description: maps[i]['description'],
-        filePath: maps[i]['filePath'],
-        createdAt: maps[i]['createdAt'],
-      );
-    });
-  }
-
+Future<List<VideoModel>> getAllVideos() async {
+  await initialiseDb();
+  final db = videoDatabase;
+  final List<Map<String, dynamic>> maps = await db.query('videos');
+  return List.generate(maps.length, (i) {
+    return VideoModel(
+      title: maps[i]['title'],
+      description: maps[i]['description'],
+      filePath: maps[i]['filePath'],
+      createdAt: maps[i]['createdAt'],
+    );
+  });
+}
